ed6b1df79d69dda38b6f813a47ff222e7dc75c15
yuedemin <yuedemin77777@163.com>
2023-02-22 19:05:08 +0800

    update mq aop

---
+++ admin/src/main/java/com/yue/ume/model/domain/User.java
@@ -0,0 +1,86 @@
+package com.yue.ume.model.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * 用户
+ * @TableName user
+ */
+@TableName(value ="user")
+@Data
+public class User implements Serializable {
+    /**
+     * 主键
+     */
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 用户名
+     */
+    private String userName;
+
+    /**
+     * 用户账号
+     */
+    private String userAccount;
+
+    /**
+     * 用户头像
+     */
+    private String avatarUrl;
+
+    /**
+     * 用户性别
+     */
+    private Integer gender;
+
+    /**
+     * 密码
+     */
+    private String userPassword;
+
+    /**
+     * 电话
+     */
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    /**
+     * 0 正常 
+     */
+    private Integer status;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    /**
+     * 用户是否存在
+     */
+    private Integer delFlag;
+
+    /**
+     * 用户角色 0普通用户 1 管理员 
+     */
+    private Integer userRole;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
